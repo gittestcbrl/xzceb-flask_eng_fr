5e88617449f87fa7ddd127f259ac08a96047ad32
gittestcbrl <crlbelgcp@gmail.com>
2022-02-04 09:59:29 +0000

    Final with unit test correction

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,11 +5,13 @@ from translator import englishToFrench, frenchToEnglish
 class TestTranslator(unittest.TestCase):
     def test_englishToFrench(self):
         self.assertEqual(englishToFrench(''), '')
+        self.assertNotEqual(englishToFrench('Bonjour'), 'help')
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
 
 
     def test_frenchToEnglish(self):
         self.assertEqual(frenchToEnglish(''), '')
+        self.assertNotEqual(frenchToEnglish('Help'), 'Bonjour')
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
 
 if __name__ == '__main__':
